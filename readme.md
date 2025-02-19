792010eaca4749df67ed3b5036b3cab64928b310
Maxim Harder <maximharder@bundeswehr.org>
2025-02-19 12:23:11 +0100

    Добавляет классы SqlTable и ParsedTable для работы с базами данных
    Создаются два новых класса: SqlTable и ParsedTable. Класс 
    SqlTable управляет метаданными таблицы, такими как имя, количество 
    записей и размер. Он включает методы для получения и установки 
    значений этих свойств, а также метод для форматирования размера.
    
    Класс ParsedTable расширяет функциональность, добавляя поддержку 
    колонок, индексов, родительских таблиц и значений. Он также 
    включает методы для работы с этими свойствами и их 
    установки/получения. Эти изменения необходимы для улучшения 
    структуры и управления данными в проекте.

--- readme.md
+++ readme.md
@@ -1,5 +1,16 @@
-просто шаблон для репозиториев
+[![GitHub issues](https://img.shields.io/github/issues/DevCraftClub/DB-Manager.svg?style=flat-square)](https://github.com/DevCraftClub/DB-Manager/issues)[![GitHub forks](https://img.shields.io/github/forks/DevCraftClub/DB-Manager.svg?style=flat-square)](https://github.com/DevCraftClub/DB-Manager/network)[![GitHub license](https://img.shields.io/github/license/DevCraftClub/DB-Manager.svg?style=flat-square)](https://github.com/DevCraftClub/DB-Manager/blob/main/LICENSE)
+# DLE Faker
 
-1. Обновить значения в manifest.json
-2. Удалить, если нет файлов локализации, файл crowdin.yml
-3. Обновляем данные в install_archive.(sh|bat), либо удаляем эти файлы
+![Текущая версия](https://img.shields.io/github/manifest-json/v/DevCraftClub/DB-Manager/main?style=for-the-badge&label=%D0%92%D0%B5%D1%80%D1%81%D0%B8%D1%8F)![Статус разработки](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fraw.githubusercontent.com%2FDevCraftClub%2FDB-Manager%2Frefs%2Fheads%2Fmain%2Fmanifest.json&query=%24.status&style=for-the-badge&label=%D0%A1%D1%82%D0%B0%D1%82%D1%83%D1%81&color=orange)
+
+![Версия DLE](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fraw.githubusercontent.com%2FDevCraftClub%2FDB-Manager%2Frefs%2Fheads%2Fmain%2Fmanifest.json&query=%24.dle&style=for-the-badge&label=DLE)![Версия PHP](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fraw.githubusercontent.com%2FDevCraftClub%2FDB-Manager%2Frefs%2Fheads%2Fmain%2Fmanifest.json&query=%24.php&style=for-the-badge&logo=php&logoColor=777BB4&label=PHP&color=777BB4)![Версия MHAdmin](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fraw.githubusercontent.com%2FDevCraftClub%2FDB-Manager%2Frefs%2Fheads%2Fmain%2Fmanifest.json&query=%24.mhadmin&style=for-the-badge&label=MH-ADMIN&color=red)
+
+# DB Manager
+
+Данный проект предназначался изначально для того, чтобы без проблем экспортировать данные с внешними ключами (Foreign Keys). Стандартный метод экспорта (от Sypex Dumper) к сожалению игнорирует данные ключи. Из-за чего возникает проблема с восстановлением данных. А поскольку я часто использую эти ключи, то частенько получаю сообщения об этом. Посему было решено написать простой плагин, который позволяет быстро экспортировать данные с внешними ключами.
+
+Обратная совместимость со стандартным методом восстановления работает без проблем. В качестве бонуса я добавил экспорт данных в канал телеграма.
+
+## Установка
+
+Процесс установки описан в [документации](https://readme.devcraft.club/latest/dev/db_manager/install/).
---
+++ upload/engine/inc/db_manager.php
@@ -0,0 +1,75 @@
+<?php
+
+//===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===
+// Mod: DB Manager
+// File: main.php
+// Path: /home/wrw-dev/Dev/Projects/dle171/engine/inc/db_manger.php
+// ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  =
+// Author: Maxim Harder <dev@devcraft.club> (c) 2025
+// Website: https://devcraft.club
+// Telegram: http://t.me/MaHarder
+// ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  =
+// Do not change anything!
+//===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===
+
+global $breadcrumbs, $mh, $modVars, $mh_template, $htmlTemplate, $config;
+
+use Symfony\Bridge\Twig\Extension\TranslationExtension;
+
+// заполняем важную и нужную информацию о модуле
+
+$modInfo = [
+	'module_name'        => 'DB Manager',
+    'module_version'     => '180.1.0',
+    'module_description' => __('Работа с базой данных, для правильного экспорта и импорта данных'),
+    'module_code'        => 'db_manager',
+    'module_id'          => 4,
+    'module_icon'        => 'fa-duotone fa-solid fa-database',
+    'site_link'          => 'https://devcraft.club',
+    'docs_link'          => 'https://devcraft.club',
+    'dle_config'         => $config,
+    'crowdin_name'       => 'db_manаger',
+    'crowdin_stat_id'    => '16830581-763961',
+
+];
+
+// Подключаем классы, функции и основные переменные
+require_once DLEPlugins::Check(__DIR__.'/maharder/admin/index.php');
+
+// Подключаем переменные модуля и его функционал
+// Используем переменную sites для навигации в модуле
+switch ($_GET['sites']) {
+	// Главная страница
+	default:
+		require_once DLEPlugins::Check(MH_ROOT.'/_modules/' . $modInfo['module_code'] . '/module/main.php');
+		break;
+
+    case 'changelog':
+        require_once DLEPlugins::Check(MH_ROOT.'/_modules/' . $modInfo['module_code'] . '/module/changelog.php');
+        break;
+
+    case 'manager':
+        require_once DLEPlugins::Check(MH_ROOT.'/_modules/' . $modInfo['module_code'] . '/module/manager.php');
+        break;
+}
+
+$mh->setLink(new AdminLink('manager', __('Управление базой данных'), '?mod=' . $modInfo['module_code'] . '&sites=manager'), 'manager');
+
+$xtraVariable = [
+	'links'       => $mh->getVariables('menu'),
+	'breadcrumbs' => $mh->getBreadcrumb(),
+	'settings'    => DataManager::getConfig($modInfo['module_code']),
+];
+
+$mh->setVars($modInfo);
+$mh->setVars($xtraVariable);
+$mh->setVars($modVars);
+
+$mh_template->addExtension(new TranslationExtension(MhTranslation::getTranslator()));
+
+// Загружаем шаблон
+$template = $mh_template->load($htmlTemplate);
+
+// Отображаем всё на сайте
+// При помощи array_merge() можно объединить любое кол-во массивов
+echo $template->render($mh->getVariables());
